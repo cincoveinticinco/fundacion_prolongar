4deec3f78a692991a5d11aed4aa2fcceeee22820
Giselle QUICENO <giselle.q@cincoveinticinco.com>
2021-06-30 18:26:56 -0500

    -

--- config/application.rb
+++ config/application.rb
@@ -10,10 +10,14 @@ module FundacionProlongar
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :es
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'America/Bogota'
+    config.active_record.default_timezone = :local
+    config.encoding = "utf-8"
   end
 end
