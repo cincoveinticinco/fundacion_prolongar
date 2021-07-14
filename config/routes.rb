39f762135a86d0870c170d07a68c1936c199f9ae
johnjejen <john.jejen@cincoveinticinco.com>
2021-07-14 15:06:52 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   get 'admin/create_home_banner/:id', to: "admin#create_home_banner"
   get 'admin/delete_home_banner/:id', to: "admin#delete_home_banner"
   get 'admin/create_contact/:id', to: "admin#create_contact"
-
-
   post 'admin/new_contacts', to:"admin#create_contacts"
 
   		namespace :admin do
