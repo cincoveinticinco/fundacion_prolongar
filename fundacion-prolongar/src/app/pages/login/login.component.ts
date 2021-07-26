7c47d7403f7423acafb43718bfc13e505c1bf412
esteban7733 <esteban.ramirez@cincoveinticinco.com>
2021-07-26 10:38:36 -0500

    cambios

--- fundacion-prolongar/src/app/pages/login/login.component.ts
+++ fundacion-prolongar/src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthservicesService } from 'src/app/services/authservices.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  model:any ={}
+  constructor(private auth:AuthservicesService) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    console.log(this.model);
+    this.auth.login(this.model).subscribe(data=>{
+      console.log(data);
+    })
+  }
 }
---
+++ fundacion-prolongar/src/app/services/authservices.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthservicesService {
+
+  public apiUrl:any=environment.url;
+
+  constructor(private http: HttpClient) { }
+
+  login(user:any) {
+    localStorage.setItem('token','1');
+    return this.http.post(`${this.apiUrl}validate_user`,user).pipe(map(resp=>{
+      let body = resp;
+      if (body.msg != "Usuario y/o contraseña invalido") {
+        
+      }
+      return body;
+    }))
+  }
+
+  logout() {}
+}
