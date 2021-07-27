02e9612296e8ab1bbc989f4441acd6f1d620fead
johnjejen <john.jejen@cincoveinticinco.com>
2021-07-27 17:39:38 -0500

    reporte excel final v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@ class User < ApplicationRecord
 
     def self.getUser
 
-      User.select('user_name,email,age, genders.gender,current_location, cities.city, receive_info,  sub_module_pages.sub_module_name as sub_module_name ')
+      User.select('user_name,email,age, genders.gender,current_location, cities.city,departments.department, receive_info, group_concat(sub_module_pages.sub_module_name) as sub_module_name ')
       .joins("inner join genders on genders.id=gender_id")
-      .joins("inner join cities on cities.id=city_id")
-      .joins("left join sub_module_page_has_users on sub_module_page_has_users.user_id=users.id")
+      .joins("left join cities on cities.id=city_id")
+      .joins("left join departments on departments.id = cities.departments_id")
+      .joins("left join sub_module_page_has_users on sub_module_page_has_users.user_id=users.id and sub_module_page_has_users.view_module = 1")
       .joins("left join sub_module_pages on sub_module_pages.id=sub_module_page_has_users.sub_module_page_id")
+      .group("users.id")
 
     end
 
