313956a62e54ca1b09b01b2e27544c8cc18d68e2
Andrés Vargas <tcarlosvargas@gmail.com>
2021-08-03 18:18:35 -0500

    .

--- fundacion-prolongar/src/app/shared/directives/validate-age.directive.ts
+++ fundacion-prolongar/src/app/shared/directives/validate-age.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import { Directive, forwardRef, Attribute } from '@angular/core';
+import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
-  selector: '[appValidateAge]'
-})
-export class ValidateAgeDirective {
+  selector: '[validateAge]',
+  providers: [{
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => ValidateAgeDirective),
+    multi: true
+  }]
 
-  constructor() { }
+})
+export class ValidateAgeDirective implements Validator{
+
+  constructor(@Attribute('maxAge') public _maxAge: string) { }
+
+  private get maxAge(): number {
+    if (!this._maxAge) return 18
+    return Number(this.maxAge)
+  }
+
+  validate(c: AbstractControl): { [key: string]: any } | null {
+    let v = Number(c.value);
+
+    if (v < this.maxAge) return {
+      validateAge: true
+    }
+
+    return null
+  }
 
 }
