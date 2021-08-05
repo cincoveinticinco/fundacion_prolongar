d66474d63a6ab6c17bd78c0de353b31150832c00
Alexander Ospina <alexander.ospina@cincoveinticinco.com>
2021-08-05 12:56:14 -0500

    .

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,9 @@
 class UserMailer < ApplicationMailer
 
-    def recovery_email(user)
+    def recovery_email(user,url_back)
         @user = user
         @url  = URL_FRONT+"/recovery_password/"+@user.recovery_password
+        @url_back = url_back
         mail(to: @user.email, subject: 'Recuperación de Contraseña')      
     end
 
