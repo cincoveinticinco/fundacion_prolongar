d7bdfbc797d1092befcc66fe0de8a9f72698e785
johnjejen <john.jejen@cincoveinticinco.com>
2021-08-05 12:12:52 -0500

    .

--- app/controllers/mailer_controller.rb
+++ app/controllers/mailer_controller.rb
@@ -4,7 +4,7 @@ class MailerController < ApplicationController
 
         params[:email] = params[:email].strip
         user = User.where('email = ?', params[:email]).take
-    
+        @url2 = URL_FRONT
           if !user.blank?
 
               token = Digest::SHA256.hexdigest([Time.now, rand].join)
